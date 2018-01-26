2fa6699e31b2801eadee10ffedd926a742e81c10
dung.hd <dung.hd@sutrixsolutions.com>
2018-01-26 18:57:19 +0700

    - headder

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,10 @@ gulp.task('css', function(){
     .pipe(less())
     .pipe(gulp.dest('build/assets/css/css'))
 });
+gulp.task('images', function(){
+  return gulp.src('app/assets/images/*')
+    .pipe(gulp.dest('build/assets/images/'))
+});
 gulp.task('css:libs', function(){
   return gulp.src('app/assets/css/libs/uikit-v2/*')
     .pipe(gulp.dest('build/assets/css/libs/uikit-v2'))
@@ -42,5 +46,5 @@ gulp.task('js:components', function(){
 });
 
 // gulp.task('default', [ 'html', 'css', 'css:libs', 'images', 'script', 'script:libs' ]);
-gulp.task('default', ['html', 'css', 'css:libs', 'css:components', 'js', 'js:lib', 'js:components' ]);
+gulp.task('default', ['html', 'css', 'css:libs', 'images', 'css:components', 'js', 'js:lib', 'js:components' ]);
 gulp.task('build', [ 'clean', 'default']);
